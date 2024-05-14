5259ecbe95a64658dbcc3feae015a91835334b47
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-05-14 16:17:25 +0530

    🧪 | added fixture

--- tests/test_cert.py
+++ tests/test_cert.py
@@ -1,29 +1,32 @@
+import pytest
 from brownie import Cert, accounts, reverts
-cert = None
 
-def test_deploy():
-    global cert
-    cert = Cert.deploy({'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
+@pytest.fixture
+def cert():
+    return Cert.deploy({'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
+
+def test_deploy(cert):
     assert cert.tx.status == 1
 
 
-def test_issue():
-    trx = cert.issue(256, 'Raven', 'MBCC', 'S', '2031-01-01',
+def test_issue(cert):
+    trx = cert.issue(256, 'Raven', 'Arcane', 'S', '2031-01-01',
                      '0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f', {'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
-    assert trx.events['Issued'].values() == [256, 'MBCC', '2031-01-01']
+    assert trx.events['Issued'].values() == [256, 'Arcane', '2031-01-01']
 
 
-def test_read():
-    certificate = cert.Certificates(256)
-    assert certificate['name'] == 'Raven'
-    assert certificate['course'] == 'MBCC'
+def test_read(cert):
+    cert.issue(512, 'Serpent', 'Umbra', 'S', '2031-01-02',
+                     '0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f', {'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
+    certificate = cert.Certificates(512)
+    assert certificate['name'] == 'Serpent'
+    assert certificate['course'] == 'Umbra'
     assert certificate['grade'] == 'S'
-    assert certificate['date'] == '2031-01-01'
+    assert certificate['date'] == '2031-01-02'
     assert certificate['document'] == '0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f'
 
 
-def test_revert():
+def test_revert(cert):
     with reverts('Not Authorized'):
-        trx = cert.issue(256, 'Enfer', 'MBCC', 'S', '2031-01-01',
+        trx = cert.issue(1024, 'Enfer', 'Catalyst', 'S', '2031-01-03',
                    '0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f', {'from': accounts[1], 'priority_fee': 10, 'max_fee': 1000000000})
-
